9d0ffc690f92a516340fb94715f434d2245e8680
Cronos Uchoa <Gabriel5432@gmail.com>
2021-02-09 22:39:38 -0300

    aprendendo conceitos de SSR - Server-side Rendering

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+//usei como referência a video aula dá rocketseat
+export default () => <div><h1>Estou Iniciando com server-side rendering (SSR) no ReactJS com Next.js</h1>
+<h3>
+Next.js - The React Framework</h3><p>"NEXT.js é mantido pela zeit e pela comunidade open-source, e visa agilizar o processo de construção de um app React oferecendo componentes para rota, suporte a SSR (server side rendering) além de webpack já configurado para React e transpilação de ES6 e ES7 que incluem recursos como async e await." </p>
+</div> 
